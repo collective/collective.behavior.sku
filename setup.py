4f4ba3c218d6d25926b317317902ac5a169fad09
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-09-02 14:59:35 +0300

    release: 0.2.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='collective.behavior.sku',
-    version='0.2.0.2',
+    version='0.2.0.3',
     description="Behavior to add sku field.",
     long_description=long_description,
     classifiers=[
